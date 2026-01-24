6f8ac20aeee7d6f5f0cbb46ab4cb2a48a1f97fca
Gaurav Srivastava <backbencherjigyasoo@gmail.com>
2026-01-24 21:20:44 +0530

    feat: Implement theme toggling functionality using React Context and dynamic CSS classes.

---
+++ toggle theme/src/context/ThemeContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeProvider, ThemeContext };
